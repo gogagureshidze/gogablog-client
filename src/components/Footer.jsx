ffbde0a4353516131ac945ec57bd36365147aa67
gogagureshidze <94310590+gogagureshidze@users.noreply.github.com>
2025-05-02 00:29:16 +0400

    minor link fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
       <div className="footer-content">
         <div className="footer-links">
           <a href="#">Home</a>
-          <a href="/#/about">About</a>
+          <a href="/gogablog-client/#/about">About</a>
           <a href="#">Services</a>
-          <a href="/#/contact">Contact</a>
+          <a href="/gogablog-client/#/contact">Contact</a>
         </div>
         <p className="footer-text">Goga Blog &copy; {currentYear}</p>
       </div>
